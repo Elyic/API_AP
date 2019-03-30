b65159bae53d84a16c632a28de3bd368ac532bd9
Elyic <fercordon87@gmail.com>
2019-03-29 22:34:12 -0600

    Agregue el metodo de registro de persona

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,13 +3,13 @@
 import  express  from 'express';
 import router from './NOMINA_TIPO_USUARIOS/NOMINA_TIPO_SOLICITUD';
 import * as pais from './PAIS/PAIS';
-
+import * as persona from './PERSONA/PERSONA';
 const app = express();
 //const express = require('express');
 
 app.use('/',router)
 app.use('/',pais.default)
-
+app.use('/',persona.default)
 
 
 export default  app;
