2f84dad1a362d31be1594b6f04fb2383992b3cb5
Elyic <fercordon87@gmail.com>
2019-05-24 02:20:18 -0600

    Varias Cosas

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,12 +4,19 @@ import  express  from 'express';
 import router from './NOMINA_TIPO_USUARIOS/NOMINA_TIPO_SOLICITUD';
 import * as pais from './PAIS/PAIS';
 import * as persona from './PERSONA/PERSONA';
+import * as categoria from './CATEGORIA/CATEGORIA';
+import * as departamento from './DEPARTAMENTO/DEPARTAMENTO';
+import * as municipio from './MUNICIPIO/MUNICIPIO';
+import * as trabajo from './TRABAJO/TRABAJO';
 const app = express();
 //const express = require('express');
 
 app.use('/',router)
 app.use('/',pais.default)
 app.use('/',persona.default)
-
+app.use('/',categoria.default)
+app.use('/',departamento.default)
+app.use('/',municipio.default)
+app.use('/',trabajo.default)
 
 export default  app;
