1d4c65ee0e90184534b0416ac8b8c4acdb73f153
Harold C <fercordon87@gmail.com>
2019-06-06 10:17:25 -0600

    Prueba bug heroku 2

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,18 +3,18 @@
 import  express  from 'express';
 import router from './NOMINA_TIPO_USUARIOS/NOMINA_TIPO_SOLICITUD';
 import * as pais from './PAIS/PAIS';
-import * as persona from './PERSONA/PERSONA';
+import { routerPERSONA} from './PERSONA/PERSONA';
 import * as categoria from './CATEGORIA/CATEGORIA';
-import * as departamento from './DEPARTAMENTO/DEPARTAMENTO';
+import { routerDEPARTAMENTO} from './DEPARTAMENTO/DEPARTAMENTO';
 import * as municipio from './MUNICIPIO/MUNICIPIO';
 import * as trabajo from './TRABAJO/TRABAJO';
 const app = express();
 //const express = require('express');
 
 app.use('/',pais.default);
-app.use('/',persona.default);
+app.use('/',routerPERSONA);
 app.use('/',categoria.default);
-app.use('/',departamento.default);
+app.use('/',routerDEPARTAMENTO);
 app.use('/',municipio.default);
 app.use('/',trabajo.default);
 app.use('/',router);
