09dfd26776454be1f31ff03b9e251fa9a2427521
Harold C <fercordon87@gmail.com>
2019-06-06 10:22:24 -0600

    Prueba bug heroku 3

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,22 +1,23 @@
 
 // export default router
 import  express  from 'express';
-import router from './NOMINA_TIPO_USUARIOS/NOMINA_TIPO_SOLICITUD';
+
 import * as pais from './PAIS/PAIS';
 import { routerPERSONA} from './PERSONA/PERSONA';
-import * as categoria from './CATEGORIA/CATEGORIA';
 import { routerDEPARTAMENTO} from './DEPARTAMENTO/DEPARTAMENTO';
-import * as municipio from './MUNICIPIO/MUNICIPIO';
-import * as trabajo from './TRABAJO/TRABAJO';
+import { routerMUNICIPIO }  from './MUNICIPIO/MUNICIPIO';
+import { routerTRABAJO} from './TRABAJO/TRABAJO';
+
+import router from './CATEGORIA/CATEGORIA';
 const app = express();
 //const express = require('express');
 
 app.use('/',pais.default);
 app.use('/',routerPERSONA);
-app.use('/',categoria.default);
+//app.use('/',categoria.default);
 app.use('/',routerDEPARTAMENTO);
-app.use('/',municipio.default);
-app.use('/',trabajo.default);
+app.use('/',routerMUNICIPIO);
+app.use('/',routerTRABAJO);
 app.use('/',router);
 
 export default  app;
