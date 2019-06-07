387b6effb9625d81681755fb574bbe22a1e3531d
Elyic <fercordon87@gmail.com>
2019-06-06 20:50:07 -0600

    F

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,8 @@
 
 // export default router
 import  express  from 'express';
-import router from './NOMINA_TIPO_USUARIOS/NOMINA_TIPO_SOLICITUD';
-import * as pais from './PAIS/PAIS';
+//import router from './NOMINA_TIPO_USUARIOS/NOMINA_TIPO_SOLICITUD';
+import router from './PAIS/PAIS';
 import * as persona from './PERSONA/PERSONA';
 import * as categoria from './CATEGORIA/CATEGORIA';
 import * as departamento from './DEPARTAMENTO/DEPARTAMENTO';
@@ -11,7 +11,7 @@ import * as trabajo from './TRABAJO/TRABAJO';
 const app = express();
 //const express = require('express');
 
-app.use('/',pais.default);
+//app.use('/',pais.default);
 app.use('/',persona.default);
 app.use('/',categoria.default);
 app.use('/',departamento.default);
